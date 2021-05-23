903c3066f1b723e5d2d089ffd5b17f9b15fbdb69
Ashish Mahindrakar <ashishmahi46@gmail.com>
2021-05-23 19:34:35 +0530

    FLUTTER: Ashish: Use FutureBuilder in order screen

--- lib/screens/oder_screen.dart
+++ lib/screens/oder_screen.dart
@@ -12,39 +12,36 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<OrderProvider>(context, listen: false)
-          .fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    final orderProvider = Provider.of<OrderProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Orders"),
       ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemBuilder: (context, index) => OrderItem(
-                order: orderProvider.orders[index],
-              ),
-              itemCount: orderProvider.orders.length,
-            ),
       drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<OrderProvider>(context, listen: false)
+            .fetchAndSetOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error fetching orders'),
+            );
+          } else {
+            return Consumer<OrderProvider>(
+                builder: (context, orderProvider, child) {
+              return ListView.builder(
+                itemBuilder: (context, index) => OrderItem(
+                  order: orderProvider.orders[index],
+                ),
+                itemCount: orderProvider.orders.length,
+              );
+            });
+          }
+        },
+      ),
     );
   }
 }
